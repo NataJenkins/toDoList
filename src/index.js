472a702a23d26f1aeb15517b04383570ee172db7
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2021-03-31 10:28:00 -0600

    Projects display js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,44 @@
-import Projects from "./projects";
+import { Projects, displayProject } from "./projects";
 import Tasks from "./tasks";
 
 const TaskUnit = (title, description, dueDate, priority) => {
   return { title, description, dueDate, priority };
 };
 
-const ProjectUnit = name => {
+const ProjectUnit = (name) => {
   return { name };
 };
 
 const myProjects = Projects();
 const myTasks = Tasks();
 const sampleProject = ProjectUnit("TODO App", "Microverse JS");
-const sampleTask = TaskUnit("JS Code", "Complete basic JS functionalities", "03/30/2021", "High");
+const sampleTask = TaskUnit(
+  "JS Code",
+  "Complete basic JS functionalities",
+  "03/30/2021",
+  "High"
+);
 myProjects.addProject(sampleProject);
 myTasks.addTask(sampleTask);
 
-const projectListElement = document.querySelector('.projects-list');
-const saveProjectBtn = document.querySelector('#save-project-btn');
+const projectListElement = document.querySelector(".projects-list");
+const saveProjectBtn = document.querySelector("#save-project-btn");
 
-window.addEventListener('click', (event) => {
+window.addEventListener("click", (event) => {
   if (event.target === saveProjectBtn) {
-    const projectName = document.querySelector('#input-project-name').value;
+    const projectName = document.querySelector("#input-project-name").value;
     const newProject = ProjectUnit(projectName);
     myProjects.addProject(newProject);
     console.log(myProjects.projectsList);
   }
 });
 
+for (let i = 0; i < myProjects.projectsList.length; i += 1) {
+  const projectCont = document.querySelector(".projects-list");
+  const projectHTML = displayProject(myProjects.projectsList[i]);
+  // projectCont.appendChild(projectHTML);
+
+  console.log(projectHTML);
+}
 
 // <li class="project-list-item"><a href="#">Project 1</a></li>
-
-
-
--- src/projects.js
+++ src/projects.js
@@ -1,14 +1,26 @@
 const Projects = () => {
-  const projectsList = {};
-  const addProject = project => {
-    projectsList[project] = project;
-  }
+  const projectsList = [];
+  const addProject = (project) => {
+    projectsList.push(project);
+  };
 
-  const removeProject = projectName => {
-    delete projectsList[projectName];
-  }
+  const removeProject = (index) => {
+    projectsLists.splice(index, 1);
+  };
 
   return { projectsList, addProject, removeProject };
-}
+};
 
-export default Projects;
+const displayProject = (project) => {
+  const projectTitle = document.createElement("li");
+  projectTitle.setAttribute("class", "project-list-item");
+
+  const projectAnc = document.createElement("a");
+  projectAnc.textContent = project.name;
+  projectAnc.setAttribute("id", "project-display");
+  projectTitle.appendChild(projectAnc);
+
+  return { projectTitle };
+};
+
+export { Projects, displayProject };
