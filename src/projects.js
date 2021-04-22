602c6dd943643d009562dee4e15e805a9f9954e4
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2021-04-22 17:41:41 -0500

    ProjectsLocalStorage

--- src/projects.js
+++ src/projects.js
@@ -17,16 +17,16 @@ const displayProject = ({ id, name }) => {
 };
 
 const Projects = () => {
-  const projectList = [
-    { name: "hola", id: 0 },
-    { name: "hola1", id: 1 },
-    { name: "hola2", id: 2 },
-    { name: "hola3", id: 3 },
-  ];
+  const getLocalStorageProjects = () =>
+    JSON.parse(localStorage.getItem("projects"));
+  const setLocalStorageProjects = (newProjectArray) => {
+    localStorage.setItem("projects", JSON.stringify(newProjectArray));
+  };
 
   const addProject = ({ name }) => {
-    const projectId = projectList.length;
-    projectList.push({ id: projectId, name });
+    let projectArray = getLocalStorageProjects();
+    const projectId = projectArray.length;
+    setLocalStorageProjects([...projectArray, { name, id: projectId }]);
     renderProjects();
   };
 
@@ -34,16 +34,16 @@ const Projects = () => {
     const projectListElement = document.querySelector(".projects-list");
     projectListElement.innerHTML = "";
 
-    projectList.forEach((project) => {
+    getLocalStorageProjects().forEach((project) => {
       projectListElement.appendChild(displayProject(project));
     });
   };
 
-  const removeProject = (index) => {
-    projectsLists.splice(index, 1);
-  };
+  // const removeProject = (index) => {
+  //   projectsLists.splice(index, 1);
+  // };
 
-  return { projectList, addProject, removeProject, renderProjects };
+  return { addProject, renderProjects };
 };
 
 export default Projects;
