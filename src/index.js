d9d0c84568effcb0dae63bd0d3d3199fb3dbc3e1
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2021-05-13 15:16:43 -0500

    index and projects refactor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import { renderProjects } from "./projects";
 import Tasks from "./tasks";
-
-//const { renderProjects } = Projects();
-
 export const changeProjectName = (name, id) => {
   currentProject = name;
   const taskHeader = document.querySelector(".tasks-container > h1");
@@ -14,26 +11,19 @@ export const changeProjectName = (name, id) => {
   document
     .querySelector(`#project-display-${id}`)
     .classList.add("selected-project");
-
   renderTaskContainer(currentProject);
   renderTasks();
 };
-
 export const filterTaskByCurrentProject = (tasksArr) =>
   tasksArr.filter((task) => task && task.project === currentProject);
-
 let currentProject = undefined;
-
 const { saveTask, renderTasks } = Tasks();
-
 if (localStorage.getItem("projects")) {
   renderProjects();
 }
-
 document.querySelector("#save-task-btn").addEventListener("click", (e) => {
   saveTask(currentProject);
 });
-
 const renderTaskContainer = (isCurrentProject) => {
   if (!isCurrentProject) {
     document.querySelector(".tasks-container").classList.add("d-none");
--- src/projects.js
+++ src/projects.js
@@ -1,59 +1,49 @@
 import { changeProjectName } from "./index";
-
 const displayProject = ({ id, name }) => {
   const projectTitle = document.createElement("li");
   const anchorElement = document.createElement("a");
-
   projectTitle.setAttribute("class", "project-list-item");
   anchorElement.setAttribute("id", `project-display-${id}`);
   anchorElement.setAttribute("href", "#");
   anchorElement.textContent = name;
-
   projectTitle.appendChild(anchorElement);
   projectTitle.addEventListener("click", () => {
     changeProjectName(name, id);
   });
   return projectTitle;
 };
-
-const Projects = () => {
-  const getLocalStorageProjects = () =>
-    JSON.parse(localStorage.getItem("projects"));
-  const setLocalStorageProjects = (newProjectArray) => {
-    localStorage.setItem("projects", JSON.stringify(newProjectArray));
-  };
-
-  const addProject = ({ name }) => {
-    let projectArray = getLocalStorageProjects();
-    if (projectArray) {
-      const projectId = projectArray.length;
-      setLocalStorageProjects([...projectArray, { name, id: projectId }]);
-    } else {
-      const projectId = 0;
-      setLocalStorageProjects([{ name, id: projectId }]);
-    }
-    renderProjects();
-  };
-
-  const renderProjects = () => {
-    const projectListElement = document.querySelector(".projects-list");
-    projectListElement.innerHTML = "";
-    getLocalStorageProjects().forEach((project) => {
-      projectListElement.appendChild(displayProject(project));
-    });
-  };
-
-  document.querySelector("#save-project-btn").addEventListener("click", (e) => {
-    const projectName = document.querySelector("#input-project-name");
-    if (projectName.value) {
-      addProject({ name: projectName.value });
-      projectName.value = "";
-    } else {
-      alert("Invalid form");
-    }
-  });
-
-  return { addProject, renderProjects };
+const getLocalStorageProjects = () => {
+  return JSON.parse(localStorage.getItem("projects"));
+  console.log(JSON.parse(localStorage.getItem("projects")));
 };
-
-export default Projects;
+const setLocalStorageProjects = (newProjectArray) => {
+  localStorage.setItem("projects", JSON.stringify(newProjectArray));
+};
+const renderProjects = () => {
+  const projectListElement = document.querySelector(".projects-list");
+  projectListElement.innerHTML = "";
+  getLocalStorageProjects().forEach((project) => {
+    projectListElement.appendChild(displayProject(project));
+  });
+};
+const addProject = ({ name }) => {
+  let projectArray = getLocalStorageProjects();
+  if (projectArray) {
+    const projectId = projectArray.length;
+    setLocalStorageProjects([...projectArray, { name, id: projectId }]);
+  } else {
+    const projectId = 0;
+    setLocalStorageProjects([{ name, id: projectId }]);
+  }
+  renderProjects();
+};
+document.querySelector("#save-project-btn").addEventListener("click", (e) => {
+  const projectName = document.querySelector("#input-project-name");
+  if (projectName.value) {
+    addProject({ name: projectName.value });
+    projectName.value = "";
+  } else {
+    alert("Invalid form");
+  }
+});
+export { addProject, renderProjects };
